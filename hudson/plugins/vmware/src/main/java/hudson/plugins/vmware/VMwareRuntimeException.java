8d7b4897a21e89205ba702d9d8292bcc63b5e89b
null <null@dev.java.net>
2007-10-01 15:53:29 +0000

    This commit was manufactured by cvs2svn to create tag 'vmware-0_2'.

---
+++ hudson/plugins/vmware/src/main/java/hudson/plugins/vmware/VMwareRuntimeException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2007 Avaya Inc.
+ *
+ * All rights reserved.
+ */
+
+package hudson.plugins.vmware;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author connollys
+ * @since 28-Sep-2007 10:16:31
+ */
+public class VMwareRuntimeException extends RuntimeException {
+    public VMwareRuntimeException() {
+        super();
+    }
+
+    public VMwareRuntimeException(String message) {
+        super(message);
+    }
+
+    public VMwareRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public VMwareRuntimeException(Throwable cause) {
+        super(cause);    
+    }
+}
