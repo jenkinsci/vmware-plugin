2164662698593b09cba1384a409d521b705aec3b
stephenconnolly <stephenconnolly@dev.java.net>
2008-06-19 05:22:47 +0000

    definine virtual computer api

---
+++ src/main/java/hudson/plugins/vmware/VirtualComputer.java
@@ -0,0 +1,8 @@
+package hudson.plugins.vmware;
+
+/**
+ * Represents a virtual computer. Virtual computers run on a virtual computer host.
+ * @author Stephen Connolly
+ */
+public interface VirtualComputer {
+}
