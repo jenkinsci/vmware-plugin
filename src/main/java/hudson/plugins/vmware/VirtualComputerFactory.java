1ce5fb01fc9584dd0c229cad48fc90f1e9b61cd9
stephenconnolly <stephenconnolly@dev.java.net>
2008-06-19 05:28:28 +0000

    definine virtual computer api

---
+++ src/main/java/hudson/plugins/vmware/VirtualComputerFactory.java
@@ -0,0 +1,9 @@
+package hudson.plugins.vmware;
+
+/**
+ * A factory for creating {@linkplain VirtualComputer}s.
+ *
+ * @author Stephen Connolly
+ */
+public abstract class VirtualComputerFactory {
+}
